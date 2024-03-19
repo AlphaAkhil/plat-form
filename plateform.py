92b8373d44526d104156da213a89acde17f14f12
NITIN PANT <97880670+AlphaAkhil@users.noreply.github.com>
2024-03-19 09:52:49 +0530

    Update plateform.py
     Note to update duntion

--- plateform.py
+++ plateform.py
@@ -4,7 +4,8 @@ import numpy as np
 import os
 
 
-
+##improve
+""" use module in place of function wheel scroll function"""
 
 #------------------------------------------------------------------------------
 # blockWidth, blockHeight
